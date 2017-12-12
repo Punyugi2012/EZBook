069e757f26370a2ba9539905c5b6c35f36af53ad
Punyugi2012 <58160216@go.buu.ac.th>
2017-12-12 14:14:35 +0700

    แก้ไขนิดหน่อย

--- EZBook/database/migrations/2017_12_11_122048_create_users_table.php
+++ EZBook/database/migrations/2017_12_11_122048_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('username');
             $table->string('password');
             $table->string('email');
-            $table->enum('type', ['publisher', 'user']);
+            $table->enum('type', ['publisher', 'user', 'admin']);
         });
     }
 
